38e25b57538ec0563a4062e4a1e8672ef3da42a6
GustavoMF31 <gustavodemendoncafreire@gmail.com>
2019-08-24 09:30:04 -0300

    Move button parsing to buttons file

--- buttons.py
+++ buttons.py
@@ -35,3 +35,23 @@ class ChangeSign(Button):
 class RemoveLastDigit(Button):
     def __init__(self):
         super().__init__(lambda x: int(str(x)[0:-1]), "<<")
+
+
+def parse_buttons(button_symbols):
+    return list(map(parse_button, button_symbols.split(" ")))
+
+
+def parse_button(button_symbol):
+
+    if button_symbol == "<<":
+        return RemoveLastDigit()
+    elif button_symbol == "+-":
+        return ChangeSign()
+    elif button_symbol.startswith("+"):
+        return Add(int(button_symbol[1:]))
+    elif button_symbol.startswith("-"):
+        return Sub(int(button_symbol[1:]))
+    elif button_symbol.startswith("X"):
+        return Mul(int(button_symbol[1:]))
+    elif button_symbol.startswith("/"):
+        return Div(int(button_symbol[1:]))
--- solveCTG.py
+++ solveCTG.py
@@ -22,34 +22,14 @@ def solveCTG(buttons, start, goal, max_moves):
 	return False, None
 
 
-def parse_buttons(button_symbols):
-	return list(map(parse_button, button_symbols.split(" ")))
-
-
-def parse_button(button_symbol):
-
-	if button_symbol == "<<":
-		return buttons.RemoveLastDigit()
-	elif button_symbol == "+-":
-		return buttons.ChangeSign()
-	elif button_symbol.startswith("+"):
-		return buttons.Add(int(button_symbol[1:]))
-	elif button_symbol.startswith("-"):
-		return buttons.Sub(int(button_symbol[1:]))
-	elif button_symbol.startswith("X"):
-		return buttons.Mul(int(button_symbol[1:]))
-	elif button_symbol.startswith("/"):
-		return buttons.Div(int(button_symbol[1:]))
-
-
 def main():
 
-	buttons = parse_buttons(input("Available buttons: "))
+	available_buttons = buttons.parse_buttons(input("Available buttons: "))
 
 	start = int(input("Starting value: "))
 	goal = int(input("Goal: "))
 	max_moves = int(input("Amount of Moves: "))
 
-	print(" ".join(solveCTG(buttons, start, goal, max_moves)[1]))
+	print(" ".join(solveCTG(available_buttons, start, goal, max_moves)[1]))
 
 main()
