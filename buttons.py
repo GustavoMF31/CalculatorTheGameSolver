86b027e45f0e619b384f8e502d8e3b36693fc806
GustavoMF31 <gustavodemendoncafreire@gmail.com>
2019-08-27 18:36:58 -0300

    Implement 'ReplaceNumber' button

--- buttons.py
+++ buttons.py
@@ -49,6 +49,18 @@ class InsertDigit(Button):
         super().__init__(lambda x: int(str(x) + str(n)), str(n))
 
 
+class ReplaceNumber(Button):
+    def __init__(self, old, new):
+        super().__init__(lambda x: int(self.replace_if_possible(str(x), old, new)), f"{old}=>{new}")
+
+    @staticmethod
+    def replace_if_possible(string, old, new):
+        if old in string:
+            return string.replace(old, new)
+        else:
+            raise ValueError(f"String '{string}' does not contain '{old}' to replace")
+
+
 def parse_buttons(button_symbols):
     return list(map(parse_button, button_symbols.split(" ")))
 
@@ -67,5 +79,9 @@ def parse_button(button_symbol):
         return Mul(int(button_symbol[1:]))
     elif button_symbol.startswith("/"):
         return Div(int(button_symbol[1:]))
+    elif "=>" in button_symbol:
+        nums = button_symbol.split("=>")
+
+        return ReplaceNumber(nums[0], nums[1])
     else:
         return InsertDigit(int(button_symbol))
